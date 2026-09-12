3f11a54041bb1516162c24a88566a9b37cdaef98
Oleksii <oleksii.syvak.25@kse.org.ua>
2026-09-12 13:41:32 +0300

    finished Shape.h, added destructor, methods, and virtual methods, which must be recreated in child classes with specific implementation

--- Shape.h
+++ Shape.h
@@ -16,14 +16,52 @@ protected:
     bool isFilled;
 
 public:
-    Shape(std::string id, int x, int y, std::string color, bool isFilled) {
-        this.id = std::move(id);
-        this.x = x;
-        this.y = y;
-        this.color = std::move(color);
-        this.isFilled = isFilled;
+    Shape(std::string id_, int x_, int y_, std::string color_, bool isFilled_) {
+        id = std::move(id_);
+        x = x_;
+        y = y_;
+        color = std::move(color_);
+        isFilled = isFilled_;
     }
-    
+
+    virtual ~Shape() = default;
+
+    std::string getId() const {
+        return id;
+    }
+
+    std::string getColor() const {
+        return color;
+    }
+
+    void setColor(const std::string& newColor) {
+        color = newColor;
+    }
+
+    int getX() const {
+        return x;
+    }
+
+    int getY() const {
+        return y;
+    }
+
+    void setPosition(int newX, int newY) {
+        x = newX;
+        y = newY;
+    }
+
+    char getColorChar() const {
+        if (color.empty()) {
+            return '*';
+        }
+        return color[0];
+    }
+
+    virtual void draw(std::vector<std::vector<char>>& grid, int boardWidth, int boardHeight) const = 0;
+    virtual bool containsPoint(int px, int py) const = 0;
+    virtual std::string getInfo() const = 0;
+    virtual std::string getType() const = 0;
 };
 
 
